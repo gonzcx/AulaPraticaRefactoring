065d8fd1e225450d12e40b1c73a679ccfb7c8a67
joaogonzales <joaovitorgonzales@gmail.com>
2022-08-07 16:00:35 -0300

    Commit inicial

---
+++ src/main/java/com/example/aula_pratica_refactoring/HelloApplication.java
@@ -0,0 +1,9 @@
+package com.example.aula_pratica_refactoring;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
