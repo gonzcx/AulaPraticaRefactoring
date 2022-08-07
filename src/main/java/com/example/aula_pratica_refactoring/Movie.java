fdb189776dc2eded6468a361e1a87ff5970714c4
joaogonzales <joaovitorgonzales@gmail.com>
2022-08-07 17:19:13 -0300

    Commit 10

--- src/main/java/com/example/aula_pratica_refactoring/Movie.java
+++ src/main/java/com/example/aula_pratica_refactoring/Movie.java
@@ -45,4 +45,12 @@ public class Movie {
         }
         return thisAmount;
     }
-}
+
+    public int getFrequentRenterPoints(int daysRented) {
+        if ((getPriceCode() == Movie.NEW_RELEASE) && daysRented > 1){
+            return 2;
+        }else{
+            return 1;
+        }
+    }
+    }
