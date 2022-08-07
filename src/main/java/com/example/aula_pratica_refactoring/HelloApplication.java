2b819d2671313632977c69f92972093c18af2173
joaogonzales <joaovitorgonzales@gmail.com>
2022-08-07 16:12:06 -0300

    Commit 1

--- src/main/java/com/example/aula_pratica_refactoring/HelloApplication.java
+++
@@ -1,9 +0,0 @@
-package com.example.aula_pratica_refactoring;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("/api")
-public class HelloApplication extends Application {
-
-}
---
+++ src/main/java/com/example/aula_pratica_refactoring/Rental.java
@@ -0,0 +1,21 @@
+package com.example.aula_pratica_refactoring;
+
+public class Rental {
+
+    private Movie _movie;
+    private int _daysRented;
+
+    public Rental(Movie movie, int daysRented) {
+        _movie = movie;
+        _daysRented = daysRented;
+    }
+
+    public int getDaysRented() {
+        return _daysRented;
+    }
+
+    public Movie getMovie() {
+        return _movie;
+    }
+}
+
